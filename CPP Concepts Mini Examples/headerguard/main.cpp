bef2ebf5c3776ecd9b7fde69f296406908dcdd0a
anuragbihani <anuragbihani@ufl.edu>
2017-06-08 14:27:51 -0400

    Addded new examples

---
+++ CPP Concepts Mini Examples/headerguard/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "add.h"
+#include "add.h" 
+
+int add(int, int);
+
+int add(int, int);
+int main()
+{
+    std::cout << "The sum of 3 and 4 is " << add(3, 4) << std::endl;
+    return 0;
+}
