edf610674089b2581a8602829cfb162a3a2ab640
Anurag Bihani <anuragbihani@ufl.edu>
2017-06-17 19:45:50 -0400

    Delete copyinit_vs_directinit.cpp

--- CPP Concepts Mini Examples/copyinit_vs_directinit.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int main()
-{
-	int val1 = 6; //copy init
-	int val2(6); //direct init
-	int val3{6}; // uniform init uncomment when using c++11
-	std::cout<<val1<<val2<<val3;
-	return 0;
-}
